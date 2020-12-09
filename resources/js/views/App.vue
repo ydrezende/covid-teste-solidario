e2fa3d5335109e99e32dbbcfa80b5b3b62b66a99
Yago Rezende <ydrdeveloper@outlook.com>
2020-12-09 13:53:59 -0300

    Primeiro commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <
+        Bem vindo! {{ user.name }}
+    </div>
+</template>
+<script>
+const default_layout = "default";
+
+
+export default {
+    computed: {},
+    data() {
+        return {
+            user:{
+                name: 'Hello World'
+            }
+        }
+    }
+};
+</script>
