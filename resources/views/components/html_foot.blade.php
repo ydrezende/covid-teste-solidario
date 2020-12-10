945a684b566fdaff551135b4192ce44719804456
Yago Rezende <ydrdeveloper@outlook.com>
2020-12-10 12:40:09 -0300

    Landing page pronta

---
+++ resources/views/components/html_foot.blade.php
@@ -0,0 +1,4 @@
+        <div id="preloader"></div>
+        <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user_client');
+    return view('welcome');
 });
 
 Route::get('/lab', function () {
